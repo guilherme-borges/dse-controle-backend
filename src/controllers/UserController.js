b38757f869883f80ebbaf6657fcea4c1157dd809
Guilherme Borges <guilherme.borges.dev@gmail.com>
2020-05-20 20:30:01 -0300

    Adicionado rotas de criação e listagem de usuários

---
+++ src/controllers/UserController.js
@@ -0,0 +1,33 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(req, res) {
+
+        const users = await connection('users').select('*')
+
+        return res.json(users)
+    },
+    async create(req, res) {
+        const { name, last_name, username, email, password } = req.body;
+
+        const user = await connection('users')
+            .select()
+            .from('users')
+            .where('username', username)
+            .first();
+
+        if (user) {
+            return res.status(400).json({ error: 'Usuário já existe!' });
+        }
+
+        const [id] = await connection('users').insert({
+            name,
+            last_name,
+            username,
+            email,
+            password
+        });
+
+        return res.status(201).json({ id })
+    }
+}
